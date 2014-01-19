ee8dc05dae6bcef7ffeba23e0cb5d767ebf822e8
Stasia Carson <stasiacarson@gmail.com>
2014-01-19 17:21:08 -0500

    Added sign-in / sign-out pages

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,9 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Required for Devise.
+  # Note to set this to actual hostname
+  config.action_mailer.default_url_options = { :host => 'sdc-pinteresting.herokuapp.com/' }
+
 end
